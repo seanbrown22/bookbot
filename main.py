672305191e9297e769b2b76c2dca4f0870b6349b
seanbrown22 <seanbrown22@gmail.com>
2025-01-13 17:10:39 -0800

    added books and deleted hello world

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hello world")
+def main():
+    with open("\\wsl.localhost\Ubuntu\home\seanbrown22\workspace\github.com\seanbrown22\bookbot\books\frankenstein.txt") as f:
+        file_contents=f.read()
+
+main()
