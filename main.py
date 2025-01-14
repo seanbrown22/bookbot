29bcc423ee5e6a5e8dfe9c5a8c8be564c953827e
seanbrown22 <seanbrown22@gmail.com>
2025-01-14 09:02:40 -0800

    added word count, character count, and character occurance

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 def main():
-    with open("\\wsl.localhost\Ubuntu\home\seanbrown22\workspace\github.com\seanbrown22\bookbot\books\frankenstein.txt") as f:
+    
+    bookname="Frankenstein"
+
+    with open("books/frankenstein.txt") as f:
         file_contents=f.read()
+
+    file_contents=file_contents.lower()
+    character_count=len(file_contents)
+    word_count=len(file_contents.split())
+    print()
+    print(f"Statistis for the book {bookname}")
+    print("---------------------------------")
+    print(f"Word count: {word_count}")
+    print(f"Character count: {character_count}")
+    from collections import Counter
+    counters=Counter(file_contents)
+
+    mykeys=list(counters.keys())
+    mykeys.sort()
+    sd={i:counters[i] for i in mykeys}
+    print()
+    print()
+    print("Occurance of each specific character:")
+    for letters in sd:
+        numbers=sd[letters]
+        print(f"{letters}: {numbers}")
 
 main()
